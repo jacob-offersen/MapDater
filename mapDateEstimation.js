7fa60bfc43cec4a57cd1ba70adb8670b152f35a8
Jacob <Offerbuffer@gmail.com>
2024-05-24 22:26:37 +0200

    Question structure updated
    The question structure updated from an object to an array to minimize overhead.
    Logic added to the "no" choice.
    Stop conditions added to stop the estimation.
    default value for upper range is now the current year

--- mapDateEstimation.js
+++ mapDateEstimation.js
@@ -8,34 +8,60 @@ const questionElementId = 'question'
 const estimatedYearElementId = 'estimatedYears'
 
 var range = {
-    "upper": 3000,
-    "lower": 0
+    "upper": new Date().getFullYear(),
+    "lower": 1900
 };
 
 var currentQuestion;
 var currentQuestionIndex;
+var estimationFinished;
+var noMoreQuestion;
 
 function pageLoaded() {
-    const questionOutput = document.getElementById(questionElementId);
-    const estimatedYearsOutput = document.getElementById(estimatedYearElementId);
-    currentQuestionIndex = 0;
-    currentQuestion = getNextQuestion();
-    questionOutput.textContent = gQuestionStrBeginning + currentQuestion.question;
-    estimatedYearsOutput.textContent = gEstimatedYearStrBeginning;
+    currentQuestionIndex = -1;
+    estimationFinished = false;
+    noMoreQuestion = false;
+    outputNextQuestion();
+    outputCurrentEstimate();
+}
+
+function handleButtonClick(choice) {
+    // Update and display new estimated range
+    if (estimationFinished == false) {
+        updateRange(choice);
+    }
+
+    // Fetch and display next question
+    if (estimationFinished == false) {
+        outputNextQuestion();
+    }
 }
 
 function updateRange(choice) {
-
+    // The goal of this estimate is to lower the upper limit as much as possible 
+    // and increase the lower limit as mich as possible. This will reduce the range as much as possible.
     switch (choice) {
         case 'yes':
+            // Check if the lower limit can be increased
             if (currentQuestion.lower != "" && currentQuestion.lower > range.lower) {
                 range.lower = currentQuestion.lower;
             }
+            // Check if the upper limit can be decreased
             if (currentQuestion.upper != "" && currentQuestion.upper < range.upper) {
                 range.upper = currentQuestion.upper;
             }
             break;
         case 'no':
+            // If the question does not have an upper range that means it is true to this present day
+            // That means the range is within the time of the question.
+            if (currentQuestion.upper != "" && range.upper > currentQuestion.lower) {
+                range.upper = currentQuestion.lower;
+            }
+            // If the question does not have a lower range that means it was true before 1900 but is no more
+            // That means the range is within the time of the question
+            if (currentQuestion.lower != "" && range.lower < currentQuestion.upper) {
+                range.lower = currentQuestion.upper;
+            }
             break;
         case 'dontknow':
             break;
@@ -43,27 +69,62 @@ function updateRange(choice) {
             break;
     }
 
-    outputCurrentEstimate();
-}
+    if (range.lower > range.upper) {
+        // If the upper limit is lower than the lower limit, something is wrong. End the estimation
+        writeToQuestionField("Invalid range. Restart the date estimation.");
+        estimationFinished = true;
 
-
-function outputCurrentEstimate() {
-    const estimatedYearsOutput = document.getElementById(estimatedYearElementId);
-    estimatedYearsOutput.textContent = gEstimatedYearStrBeginning + range.lower + "-" + range.upper;
+    } else if (range.lower == range.upper) {
+        // If the limits are the same the range as reached a single year. End the estimation
+        writeToEstimateField(range.lower);
+        estimationFinished = true;
+    } else {
+        // The estimation can be improved keep going
+        outputCurrentEstimate();
+    }
 }
 
 function getNextQuestion() {
+    question = []
     currentQuestionIndex = currentQuestionIndex + 1;
-    return questions[currentQuestionIndex];
+    if (currentQuestionIndex != questions.length) {
+        // Extract the new question
+        question = questions[currentQuestionIndex]
+    } else {
+        // There are no more questions
+        noMoreQuestion = true;
+    }
+    return question;
 }
 
 function outputNextQuestion() {
-    const questionOutput = document.getElementById(questionElementId);
-    currentQuestion = getNextQuestion();
-    questionOutput.textContent = gQuestionStrBeginning + currentQuestion.question;
+    // Get next question
+    const question = getNextQuestion();
+    currentQuestion = {
+        question: question[questionIndex],
+        lower: question[lowerIndex],
+        upper: question[upperIndex]
+    };
+    if (false == noMoreQuestion) {
+        // Display new question
+        writeToQuestionField(gQuestionStrBeginning + currentQuestion.question);
+    } else {
+        // There are no more questions to ask. The estimation cannot be further improved. End the estimation.
+        writeToQuestionField("No more questions. Estimation cannot be improved.");
+        estimationFinished = true;
+    }
 }
 
-function handleButtonClick(choice) {
-    updateRange(choice);
-    outputNextQuestion();
+function outputCurrentEstimate() {
+    writeToEstimateField(gEstimatedYearStrBeginning + range.lower + "-" + range.upper);
+}
+
+function writeToEstimateField(string) {
+    const estimatedYearsOutput = document.getElementById(estimatedYearElementId);
+    estimatedYearsOutput.textContent = string
+}
+
+function writeToQuestionField(string) {
+    const questionOutput = document.getElementById(questionElementId);
+    questionOutput.textContent = string;
 }
--- questions.js
+++ questions.js
@@ -1,35 +1,13 @@
-const questions = [{
-        question: "Is the soviet union present?",
-        lower: 1922,
-        upper: 1991
-    },
-    {
-        question: "Is Sudan present? (Not South Sudan)",
-        lower: 2011,
-        upper: ""
-    },
-    {
-        question: "Is Istanbul present?",
-        lower: 1930,
-        upper: ""
-    },
-    {
-        question: "Is Germany united?",
-        lower: 1989,
-        upper: ""
-    },
-    {
-        question: "Is there a railway linking Moscow and Vladivostok?",
-        lower: 1904,
-        upper: ""
-    },
-    {
-        question: "Is Yugoslavia present?",
-        lower: 1918,
-        upper: 1992
-    },
-    // {question:"", lower: , upper: },
-    // {question:"", lower: , upper: },
-    // {question:"", lower: , upper: },
-    //{question:"", lower: , upper: },
+const questionIndex = 0;
+const lowerIndex = 1;
+const upperIndex = 2;
+
+const questions = [
+    ["Is the Soviet Union present?", 1922, 1991],
+    ["Is Sudan present? (Not South Sudan)", 2011, ""],
+    ["Is Istanbul present?", 1930, ""],
+    ["Is Germany united?", 1989, ""],
+    ["Is there a railway linking Moscow and Vladivostok?", 1904, ""],
+    ["Is Yugoslavia present?", 1918, 1992],
+    // ["", , ],
 ];
